02dd28c0c450641c614af8a43e500d5a0acbec38
Andrea Crotti <kerny404@gmail.com>
2010-04-08 13:21:50 +0200

    more code and text

---
+++ orgloader.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path "~/Documents/pycon/conf/org-mode/lisp")
+(add-to-list 'load-path "~/Documents/pycon/conf/org-mode/contrib/babel/lisp")
+
+(require 'org)
+(require 'org-babel)
+(require 'org-babel-tangle)
+;; (require 'org-babel-python)
