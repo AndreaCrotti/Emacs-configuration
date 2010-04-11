f4a551ba0bb5894f9552d103e79a6a0b40f4d24f
Andrea Crotti <kerny404@gmail.com>
2010-04-11 16:30:53 +0200

    now loader works correctly, was a path missing

--- orgloader.el
+++ orgloader.el
@@ -3,9 +3,11 @@
 
 (add-to-list 'load-path (concat conf "org-mode/lisp"))
 (add-to-list 'load-path (concat conf "org-mode/contrib/babel/lisp"))
+(add-to-list 'load-path (concat conf "org-mode/contrib/babel/lisp/langs"))
 
 (require 'org)
 (require 'org-babel)
 (require 'org-babel-tangle)
+(require 'org-babel-init)
 
 (org-babel-load-file (concat base "miniconf.org"))
