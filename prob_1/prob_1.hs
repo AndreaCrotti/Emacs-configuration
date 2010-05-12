84cc1c0aa082000dced19e8d890b2d0f10d580a6
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-05-12 17:46:52 +0200

    adding the snippets

--- prob_1/prob_1.hs
+++
@@ -1,3 +0,0 @@
-
-main = do
-  print $ sum $ filter (\x -> (mod x 3 == 0) || (mod x 5 == 0)) [0..1000]
--- prob_1/prob_1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# Add all the natural numbers below one thousand that are multiples of 3 or 5
-
-# OR is short circuiting, putting n % 3 before n % 5 can already
-# greatly reduce the number of calls
-# Using directly a generator instead of list comprehension is always a good thing
-print sum(n for n in xrange(1000) if (n % 3 == 0 or n % 5 == 0))    
