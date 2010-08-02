55b9b27c69aa848b83493a19895752c2137a8d8d
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-02 22:17:13 +0200

    readding necessary org files

---
+++ conf/site-lisp/virtual.el
@@ -0,0 +1,41 @@
+;;; virtual.el --- Support for virtualenv for python-mode.el
+
+;; Copyright (C) 2010  Andrea Crotti
+
+;; Author: Andrea Crotti <andrea.crotti.0@gmail.com>
+;; Keywords: languages
+
+;; TODO: change exec path also
+;; TODO: make it less intrusive (don't use the global variable)
+
+(require 'python-mode)
+
+(defun virtual-set-interpreter ()
+  (interactive)
+  (let
+      ((interpreter (expand-file-name "bin/python")))
+    (if 
+        (file-exists-p "bin/python")
+        (progn
+          (message "setting the local interpreter")
+          (setq py-python-command interpreter)
+          (add-to-list 'exec-path (expand-file-name "bin")))
+      (message "not possible to setup the environment"))))
+
+(defun virtual-deactivate ()
+  (interactive)
+  (message "deactivate the environment")
+  ;; TODO: add a way to revert the exec path change
+  (py-toggle-shells 1))
+
+(setq paster-shell "./bin/paster shell ldapper/development.ini")
+
+(defun run-turbogears ()
+  "Run an interactive turbogears shell"
+  (interactive)
+  (require 'comint)
+  (switch-to-buffer (make-comint "turbogears" paster-shell))
+  ;; now maybe pass to the right mode
+)
+
+(provide 'virtual)
