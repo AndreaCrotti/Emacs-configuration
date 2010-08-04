a1e2ca3c17fc47342a8c3f77bd775507ca8fb34a
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-04 14:37:59 +0200

    swank and my first minor mode

---
+++ conf/swank-clojure/project.clj
@@ -0,0 +1,5 @@
+(defproject swank-clojure "1.1.0"
+  :description "Swank server connecting Clojure to Emacs SLIME"
+  :url "http://github.com/technomancy/swank-clojure"
+  :main swank.swank
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]])
