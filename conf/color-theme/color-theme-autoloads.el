bb9b373d99926543429bc3f1710e1adc44c3eda5
Andrea Crotti <kerny404@gmail.com>
2010-08-09 14:26:29 +0200

    ielm-mode with eldoc-mode

--- conf/color-theme/color-theme-autoloads.el
+++ conf/color-theme/color-theme-autoloads.el
@@ -15,7 +15,7 @@
 ;;; Generated autoloads follow (made by autoload.el).
 
 ;;;### (autoloads nil nil ("themes/color-theme-example.el" "themes/color-theme-library.el")
-;;;;;;  (19105 25322 64333))
+;;;;;;  (19551 38368 16411))
 
 ;;;***
 
@@ -23,10 +23,10 @@
 ;;;;;;  color-theme-compare color-theme-make-snapshot color-theme-analyze-defun
 ;;;;;;  color-theme-print color-theme-install-at-point-for-current-frame
 ;;;;;;  color-theme-install-at-mouse color-theme-describe color-theme-select)
-;;;;;;  "color-theme" "color-theme.el" (17529 41105))
+;;;;;;  "color-theme" "color-theme.el" (19440 632))
 ;;; Generated autoloads from color-theme.el
 
-(autoload (quote color-theme-select) "color-theme" "\
+(autoload 'color-theme-select "color-theme" "\
 Displays a special buffer for selecting and installing a color theme.
 With optional prefix ARG, this buffer will include color theme libraries
 as well.  A color theme library is in itself not complete, it must be
@@ -35,7 +35,7 @@ libraries are mainly useful for color theme authors.
 
 \(fn &optional ARG)" t nil)
 
-(autoload (quote color-theme-describe) "color-theme" "\
+(autoload 'color-theme-describe "color-theme" "\
 Describe color theme listed at point.
 This shows the documentation of the value of text-property color-theme
 at point.  The text-property color-theme should be a color theme
@@ -43,21 +43,21 @@ function.  See `color-themes'.
 
 \(fn)" t nil)
 
-(autoload (quote color-theme-install-at-mouse) "color-theme" "\
+(autoload 'color-theme-install-at-mouse "color-theme" "\
 Install color theme clicked upon using the mouse.
 First argument EVENT is used to set point.  Then
 `color-theme-install-at-point' is called.
 
 \(fn EVENT)" t nil)
 
-(autoload (quote color-theme-install-at-point-for-current-frame) "color-theme" "\
+(autoload 'color-theme-install-at-point-for-current-frame "color-theme" "\
 Install color theme at point for current frame only.
 Binds `color-theme-is-global' to nil and calls
 `color-theme-install-at-point'.
 
 \(fn)" t nil)
 
-(autoload (quote color-theme-print) "color-theme" "\
+(autoload 'color-theme-print "color-theme" "\
 Print the current color theme function.
 
 You can contribute this function to <URL:news:gnu.emacs.sources> or
@@ -86,7 +86,7 @@ Example:
 
 \(fn &optional BUF)" t nil)
 
-(autoload (quote color-theme-analyze-defun) "color-theme" "\
+(autoload 'color-theme-analyze-defun "color-theme" "\
 Once you have a color-theme printed, check for missing faces.
 This is used by maintainers who receive a color-theme submission
 and want to make sure it follows the guidelines by the color-theme
@@ -94,13 +94,13 @@ author.
 
 \(fn)" t nil)
 
-(autoload (quote color-theme-make-snapshot) "color-theme" "\
+(autoload 'color-theme-make-snapshot "color-theme" "\
 Return the definition of the current color-theme.
 The function returned will recreate the color-theme in use at the moment.
 
 \(fn)" nil nil)
 
-(autoload (quote color-theme-compare) "color-theme" "\
+(autoload 'color-theme-compare "color-theme" "\
 Compare two color themes.
 This will print the differences between installing THEME-A and
 installing THEME-B.  Note that the order is important: If a face is
@@ -111,7 +111,7 @@ a difference.
 
 \(fn THEME-A THEME-B)" t nil)
 
-(autoload (quote color-theme-install) "color-theme" "\
+(autoload 'color-theme-install "color-theme" "\
 Install a color theme defined by frame parameters, variables and faces.
 
 The theme is installed for all present and future frames; any missing
@@ -146,12 +146,12 @@ frame-parameter settings of previous color themes.
 
 \(fn THEME)" nil nil)
 
-(autoload (quote color-theme-submit) "color-theme" "\
+(autoload 'color-theme-submit "color-theme" "\
 Submit your color-theme to the maintainer.
 
 \(fn)" t nil)
 
-(autoload (quote color-theme-initialize) "color-theme" "\
+(autoload 'color-theme-initialize "color-theme" "\
 Initialize the color theme package by loading color-theme-libraries.
 
 \(fn)" t nil)
