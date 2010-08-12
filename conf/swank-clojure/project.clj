3215c4026b16e0ff7e7c13edf11831e1a0374a33
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-12 11:57:13 +0200

    Deleting all the old stuff in "conf" and using submodules instead

--- conf/swank-clojure/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject swank-clojure "1.1.0"
-  :description "Swank server connecting Clojure to Emacs SLIME"
-  :url "http://github.com/technomancy/swank-clojure"
-  :main swank.swank
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]])
