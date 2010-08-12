5db159d36395c0f3b0872eb925f858cbe381afeb
Andrea Crotti <andrea.crotti.0@gmail.com>
2010-08-12 11:57:53 +0200

    Readding cedet

---
+++ cedet/www/download.php
@@ -0,0 +1,34 @@
+<!-- -*-html-*- -->
+<br clear="all">
+<p>
+<table width="100%" class="BAR"><tr><td>
+<h2>Downloading CEDET</h2>
+</td></tr></table>
+
+<p>All the <b>CEDET</b> tools are available from a single distribution file.</p>
+
+<p><b>CEDET</b> is currently driving toward a 1.0 release.  Try out a
+pre-release and send in bug reports on the build process, or anything
+else to the <a href="http://lists.sourceforge.net/lists/listinfo/cedet-devel">mailing list</a>.
+
+<p>Try out
+<a href="https://sourceforge.net/projects/cedet/files/cedet/1.0pre7/cedet-1.0pre7.tar.gz/download">
+cedet-1.0pre7.tar.gz</a>.
+
+<p>After building <b>CEDET</b>, consider posting to
+the <a href="http://lists.sourceforge.net/lists/listinfo/cedet-devel">mailing
+list</a> to let us know how it went!  I'm most interested in
+platform/Emacs version information.
+
+<p><b>Please Note:</b>
+
+<p>If you encounter build problems with a CEDET release, those issues
+  may have already been fixed in CVS!  CEDET has an active community
+  of users that help identify and fix these issues quickly.  You can check the
+  <a href="https://sourceforge.net/mailarchive/forum.php?forum_name=cedet-devel">
+  mailing list archives</a> or just try the
+  <a href="https://sourceforge.net/scm/?type=cvs&group_id=17886">
+  CVS version</a> directly.
+
+
+
