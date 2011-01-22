c26166266882f1126db67c8dac82530da0ec3da1
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-01-22 16:16:54 +0100

    tramp path must be set before anything else, check if it's possible to do in another way

--- orgloader.el
+++ orgloader.el
@@ -1,6 +1,9 @@
 (defun make-conf-path (path)
   (expand-file-name (concat base path)))
 
+(add-to-list 'load-path (make-conf-path "tramp/lisp"))
+
+
 (add-to-list 'load-path (make-conf-path "org-mode/lisp"))
 (add-to-list 'load-path (make-conf-path "org-mode/contrib/babel/lisp"))
 (add-to-list 'load-path (make-conf-path "org-mode/contrib/babel/lisp/langs"))
