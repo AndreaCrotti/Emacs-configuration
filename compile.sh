9f92f304761d60bcf88ff9b5082756081552f79d
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-07-16 01:55:03 +0200

    new script to attempt a full compilation of all the needed submodules

--- compile.sh
+++ compile.sh
@@ -2,4 +2,26 @@
 # need to compile
 # - tramp
 # - doxymacs
-# - 
+# - org-mode
+# - gnus
+# - Pymacs
+
+TO_COMPILE="tramp doxymacs org-mode gnus Pymacs magit cedet"
+
+set -x 
+
+function smart_compile {
+    if [ -f configure.ac]
+    then 
+        if [ ! -x configure ]
+        then autoreconf -fi
+        else ./configure
+        fi
+    fi
+    make
+}
+
+for submod in $TO_COMPILE
+# is the path changed and automatically fixed or not?
+do (cd $submod && smart_compile)
+done
