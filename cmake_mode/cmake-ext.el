fd706b9a07c9ceb79a70194848f322eb9c3e25f3
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-10-20 16:10:25 +0100

    refactor the helper code

--- cmake_mode/cmake-ext.el
+++ cmake_mode/cmake-ext.el
@@ -13,12 +13,13 @@
   "Show the variable or function documentation"
   (interactive)
   (let 
-      ((word (thing-at-point 'symbol))
-       (help-string "command %s:\n%s"))
+      ((word (thing-at-point 'symbol)))
     (with-temp-buffer
-      (insert
-       (format help-string
-               word (shell-command-to-string (format "cmake --help-variable %s" word)))))))
+      ;TODO: check if
+      ; 1. it's a variable
+      ; 2. it's a function
+      ; 3. if it's none just return an error
+      (insert (shell-command (format "cmake --help-variable %s" word))))))
 
 (setq cmake-command-list
       '("add_custom_command" "add_custom_target" "add_definitions" "add_dependencies" "add_executable" "add_library" "add_subdirectory" "add_test" "aux_source_directory" "break" "build_command" "cmake_minimum_required" "cmake_policy" "configure_file" "create_test_sourcelist" "define_property" "else" "elseif" "enable_language" "enable_testing" "endforeach" "endfunction" "endif" "endmacro" "endwhile" "execute_process" "export" "file" "find_file" "find_library" "find_package" "find_path" "find_program" "fltk_wrap_ui" "foreach" "function" "get_cmake_property" "get_directory_property" "get_filename_component" "get_property" "get_source_file_property" "get_target_property" "get_test_property" "if" "include" "include_directories" "include_external_msproject" "include_regular_expression" "install" "link_directories" "list" "load_cache" "load_command" "macro" "mark_as_advanced" "math" "message" "option" "project" "qt_wrap_cpp" "qt_wrap_ui" "remove_definitions" "return" "separate_arguments" "set" "set_directory_properties" "set_property" "set_source_files_properties" "set_target_properties" "set_tests_properties" "site_name" "source_group" "string" "target_link_libraries" "try_compile" "try_run" "unset" "variable_watch" "while" "build_name" "exec_program" "export_library_dependencies" "install_files" "install_programs" "install_targets" "link_libraries" "make_directory" "output_required_files" "remove" "subdir_depends" "subdirs" "use_mangled_mesa" "utility_source" "variable_requires" "write_file"))
