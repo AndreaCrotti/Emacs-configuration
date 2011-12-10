dbf58743f341753fd337b4dbdf7d52643bb992f8
Andrea Crotti <andrea.crotti.0@gmail.com>
2011-12-10 21:23:17 +0000

    fix a couple of wrong destinations

--- modules/ca-functions.el
+++ modules/ca-functions.el
@@ -427,6 +427,31 @@ Otherwise, expand the current region to select the lines the region touches."
 (defun ca-uncamel ()
   (ca-manipulate-matched-text 'un-camelcase-string))
 
+(defun ca-untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun ca-indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun ca-cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (ca-indent-buffer)
+  (ca-untabify-buffer)
+  (delete-trailing-whitespace))
+
+; FIXME: previous-line should be only used as interactive function
+(defun ca-ditto ()
+  "*Copy contents of previous line, starting at the position above point."
+  (interactive)
+  (let ((last-command nil))
+    (save-excursion
+      (previous-line 1)
+      (copy-region-as-kill (point) (progn (end-of-line) (point))))
+    (yank 1)))
+
 ;; TODO: add support for different modes
 (defun ca-look-for-function ()
   (interactive)
