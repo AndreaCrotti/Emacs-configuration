71e69df943a26fefbb79823667e3d6fbe47fdfc1
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-01-03 16:52:04 +0000

    use hooks instead of just loading

--- modules/ca-faces.el
+++ modules/ca-faces.el
@@ -18,10 +18,14 @@
             (local-set-key (kbd "M-n") 'rst-forward-section)
             (local-set-key (kbd "M-p") 'rst-backward-section)))
 
-(set-face-attribute 'diff-changed nil
-                    :background "dark red")
+(add-hook 'diff
+          (lambda ()
+            (set-face-attribute 'diff-changed nil
+                                :background "dark red")))
 
-(set-face-attribute 'magit-item-highlight nil
-                    :weight 'bold :height 1.2)
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (set-face-attribute 'magit-item-highlight nil
+                                :weight 'bold :height 1.2)))
 
 (provide 'ca-faces)
