8eb76fb9392573877f1240fe8c771d51a2e7dc54
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-02-19 22:53:01 +0000

    add some nice functions to use package.el shipped with emacs 24

--- modules/ca-packages.el
+++ modules/ca-packages.el
@@ -1,5 +1,35 @@
+(require 'package)
+(package-initialize)
 
-; Other autoloads
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(defvar ca-packages
+  '(ack-and-a-half auctex clojure-mode coffee-mode deft expand-region
+                   gist groovy-mode haml-mode haskell-mode inf-ruby
+                   magit magithub markdown-mode paredit projectile python
+                   sass-mode rainbow-mode scss-mode solarized-theme
+                   volatile-highlights yaml-mode yari zenburn-theme)
+  "A list of packages to ensure are installed at launch.")
+
+(defun ca-packages-installed-p ()
+  (loop for p in ca-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(defun ca-install-missing-packages ()
+  (interactive)
+  (unless (ca-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Emacs packages is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (dolist (p ca-packages)
+      (when (not (package-installed-p p))
+        (package-install p)))))
+
+
 (autoload 'auto-install-from-emacswiki "auto-install" "auto install from emacswiki" t)
 (setq auto-install-directory (make-conf-path "auto-install/"))
 
