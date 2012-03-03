b61242aabfe7282766cc7bdcfa2c4eecc0484029
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-03-03 20:55:06 +0000

    ocaml fix

--- modules/ca-ocaml.el
+++ modules/ca-ocaml.el
@@ -6,13 +6,15 @@
 (autoload 'typerex-mode "typerex" "Major mode for editing Caml code" t)
 
 ;; TypeRex mode configuration
-(setq ocp-server-command "/usr/bin/ocp-wizard")
-(setq typerex-in-indent 0)
-(setq-default indent-tabs-mode nil)
+(setq
+ ocp-auto-complete t
+ ocp-theme "tuareg_like"
+ typerex-library-path "/usr/lib/ocaml"
+ ocp-server-command "/usr/bin/ocp-wizard"
+ typerex-in-indent 0)
 
-(setq typerex-library-path "/usr/lib/ocaml")
-(setq ocp-theme "tuareg_like")
-(setq ocp-auto-complete t)
+
+(setq-default indent-tabs-mode nil)
 
 ;not necessary if all the dicts are already there
 ;; (add-to-list 'ac-dictionary-directories "/usr/share/emacs/site-lisp/auto-complete-mode/ac-dict")
