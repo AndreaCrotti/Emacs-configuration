7d40dc7d868c2058398762bcf8dfe4511277580f
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-04-29 13:32:45 +0100

    cleanup unused rfc code

--- modules/ca-network.el
+++ modules/ca-network.el
@@ -1,18 +1,14 @@
 (require 'ca-erc)
 (require 'ca-mail)
 
-(setq
- rfc-url-save-directory "~/rfc"
- rfc-insert-content-url-hook '(rfc-url-save)
- rfc-index-url "http://www.ietf.org/iesg/1rfc_index.txt"
- rfc-archive-alist (list (concat rfc-url-save-directory "/rfc.zip")
-                         rfc-url-save-directory
-                         "http://www.ietf.org/rfc/"))
-
 (require 'irfc)
 (setq irfc-directory "~/rfcs")
+
+(when (not (file-directory-p irfc-directory))
+  (message "creating directory %s to store the rfc files" irfc-directory)
+  (make-directory irfc-directory))
+
 (add-to-list 'auto-mode-alist
              '("/rfc[0-9]+\\.txt\\'" . irfc-mode))
-
 
 (provide 'ca-network)
