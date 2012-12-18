0ebcc71efbc1a50c461278fa69641f52e2056bee
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-12-18 20:55:52 +0000

    require css and coffee mode

--- modules/ca-web.el
+++ modules/ca-web.el
@@ -1,6 +1,8 @@
 ;; web framework things
 (add-to-list 'load-path (make-conf-path "pony-mode/src"))
 
+(require 'coffee-mode)
+(require 'less-css-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("\\.iced$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
