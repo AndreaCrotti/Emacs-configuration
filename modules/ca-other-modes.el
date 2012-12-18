51ff6fe7a03e1dacf8afeb3b210779b58d7df140
Andrea Crotti <andrea.crotti.0@gmail.com>
2012-12-18 21:41:11 +0000

    other modes

--- modules/ca-other-modes.el
+++ modules/ca-other-modes.el
@@ -240,7 +240,7 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 
-(setq auto-mode-alist
-      (cons '("\\.text" . markdown-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.text" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown" . markdown-mode))
 
 (provide 'ca-other-modes)
