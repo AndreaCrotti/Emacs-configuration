b5a42f8af03c933039dc88c4bff5b88cc9ab8e6e
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-03-22 14:07:51 +0000

    use emmet mode

--- modules/ca-web.el
+++ modules/ca-web.el
@@ -3,4 +3,9 @@
 (require 'less-css-mode)
 (add-to-list 'auto-mode-alist '("\\.less" . less-css-mode))
 
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'html-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook  'emmet-mode)
+
 (provide 'ca-web)
