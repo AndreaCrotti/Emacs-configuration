b35c0c0164689ec71c2bbc78d76d192cd1c155be
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-12-13 19:39:30 +0000

    more improvements

--- modules/ca-clojure.el
+++ modules/ca-clojure.el
@@ -1,15 +1,18 @@
 (require 'cider)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
-(add-to-list 'auto-mode-alist '("\\.clj$" . cider-mode))
 (autoload 'clojure-mode "clojure-mode" "clojure mode" t)
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
+(add-hook 'clojure-mode-hook 'cider-mode)
 (setq nrepl-log-messages t)
 (setq cider-repl-use-clojure-font-lock t)
 
+; cider mode works better with company-mode than ac apparently
 (require 'company)
 (add-hook 'cider-repl-mode-hook 'company-mode)
 (add-hook 'cider-mode-hook 'company-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'cider-turn-on-eldoc-mode)
 
 ;; (autoload 'slime "swank-clojure" "loading the swank-clojure" t)
 ;; (add-hook 'slime-mode-hook
