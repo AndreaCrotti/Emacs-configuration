8ebb9b725de43b3aee82703414682e4f168212c4
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-12-18 18:52:53 +0000

    send region binding

--- modules/ca-javascript.el
+++ modules/ca-javascript.el
@@ -1,13 +1,14 @@
 (require 'js-comint)
 
-(setq inferior-js-program-command "node")
+(setq inferior-js-program-command "node --interactive")
 
 (setenv "NODE_NO_READLINE" "1")
 
-(add-hook 'js2-mode-hook '(lambda ()
+(add-hook 'js-mode-hook '(lambda ()
                             (local-set-key "\C-x\C-e" 'js-send-last-sexp)
                             (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
                             (local-set-key "\C-cb" 'js-send-buffer)
+                            (local-set-key "\C-c\C-r" 'js-send-region)
                             (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
                             (local-set-key "\C-cl" 'js-load-file-and-go)
                             ))
