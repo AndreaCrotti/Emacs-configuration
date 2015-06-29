781040e361a0e1bce7d747582dd1ccb4cadd1fb7
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-29 09:33:28 +0100

    add custom el

---
+++ custom.el
@@ -0,0 +1,26 @@
+(load-theme 'noctilux)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cider-lein-command "/home/andrea/.local/bin/lein")
+ '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(display-time-mode t)
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yaml-mode web-mode virtualenvwrapper virtualenv tuareg tramp-hdfs textile-mode terraform-mode tdd table smartparens smart-mode-line shrink-whitespace rinari restclient rebox2 rainbow-mode rainbow-delimiters pytest puppet-mode psvn powerline pony-mode pomodoro pinentry php-mode p4 org-present org-jekyll org-gcal org-dashboard org-bullets offlineimap nyan-mode notmuch noctilux-theme monky minimap memory-usage markdown-mode marcopolo malabar-mode magit lua-mode log4j-mode less-css-mode kanban jvm-mode js-comint jedi inf-clojure indent-guide htmlize hi2 heroku helm-projectile haskell-mode hackernews groovy-mode graphviz-dot-mode google-translate google-contacts go-mode github-browse-file git-timemachine git-gutter gist flycheck find-file-in-repository evil eval-in-repl ess erlang ensime emms emmet-mode elixir-mode elein ein edit-server ebib dpaste dockerfile-mode docker-tramp discover-clj-refactor dired-details dired+ d-mode csv-mode csharp-mode company-anaconda color-theme color-moccur cmake-mode clojure-cheatsheet c-eldoc butler browse-kill-ring bookmark+ bitlbee batch-mode autopair auctex ascii arduino-mode apache-mode android-mode adoc-mode adaptive-wrap ack ac-slime ac-js2 4clojure)))
+ '(safe-local-variable-values (quote ((ca-cleanup-is-enabled . t))))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
