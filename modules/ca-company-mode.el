ebe1c71dd2cb1a5c89bb4030c510a5b70abbb20b
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-08-20 11:07:57 +0100

    more company mode ideas

---
+++ modules/ca-company-mode.el
@@ -0,0 +1,9 @@
+(require 'company)
+
+;; TODO: something like this but this binds it too much, so need something
+;; that would only bind to the company mode sub environment
+;; (define-key company-mode-map "\C-n" 'company-select-next)
+
+;; (define-key company-mode-map "\C-p" 'company-select-previous)
+
+(provide 'ca-company-mode)
