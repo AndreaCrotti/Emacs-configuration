25eb4512f86649212c025d234212e812a28fb789
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-10-29 21:01:07 +0000

    settings

--- custom.el
+++ custom.el
@@ -1,4 +1,4 @@
-
+(setq org-reveal-root "/home/andrea/projects/oss_projects/javascript/reveal.js/")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,7 +19,7 @@
     (eval-in-repl clj-refactor zenburn-theme yaml-mode web-mode virtualenvwrapper virtualenv tuareg textile-mode smartparens smart-mode-line sly-company rinari rebox2 rainbow-mode rainbow-delimiters puppet-mode psvn powerline pony-mode pomodoro php-mode p4 org-present org-jekyll org-gcal org-bullets offlineimap notmuch noctilux-theme multiple-cursors monky minimap memory-usage markdown-mode malabar-mode magit lua-mode log4j-mode less-css-mode kanban js-comint jedi inf-clojure indent-guide htmlize hi2 heroku helm-projectile haskell-mode hackernews groovy-mode graphviz-dot-mode google-translate google-contacts go-mode git-timemachine git-gutter gist flycheck find-file-in-repository evil ess erlang ensime emms emmet-mode elixir-mode elein ein edit-server ebib dpaste dockerfile-mode dired-details dired+ d-mode csv-mode csharp-mode color-theme color-moccur cmake-mode clojure-cheatsheet c-eldoc browse-kill-ring bookmark+ bitlbee batch-mode autopair auctex ascii arduino-mode apache-mode android-mode adoc-mode ack ac-slime ac-js2 4clojure)))
  '(paradox-github-token t)
  '(safe-local-variable-values (quote ((ca-cleanup-is-enabled . t))))
- '(with-editor-emacsclient-executable "/usr/bin/emacsclient-snapshot"))
+ '(with-editor-emacsclient-executable "/usr/bin/emacsclient"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
