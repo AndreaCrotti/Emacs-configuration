b8d8909fd4a03e55eb34afcf0f6a5e4c7ebb8cb9
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-11-18 09:41:47 +0000

    theme and a few more things

--- custom.el
+++ custom.el
@@ -1,22 +1,24 @@
+(load-theme 'noctilux)
+(set-default-font "-unknown-Ubuntu Mono-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1")
 (setq org-reveal-root "/home/andrea/projects/oss_projects/javascript/reveal.js/")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(cider-lein-command "/home/andrea/.local/bin/lein")
  '(custom-safe-themes
    (quote
     ("0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(ein:use-auto-complete-superpack t)
- '(jedi:server-command
+ '(org-babel-load-languages
    (quote
-    ("python" "/home/andrea/.emacs.d/elpa/jedi-core-20150623.2335/jediepcserver.py")))
- '(magit-push-always-verify nil)
+    ((python . t)
+     (haskell . t)
+     (emacs-lisp . t)
+     (clojure . t))))
  '(package-selected-packages
    (quote
-    (eval-in-repl clj-refactor zenburn-theme yaml-mode web-mode virtualenvwrapper virtualenv tuareg textile-mode smartparens smart-mode-line sly-company rinari rebox2 rainbow-mode rainbow-delimiters puppet-mode psvn powerline pony-mode pomodoro php-mode p4 org-present org-jekyll org-gcal org-bullets offlineimap notmuch noctilux-theme multiple-cursors monky minimap memory-usage markdown-mode malabar-mode magit lua-mode log4j-mode less-css-mode kanban js-comint jedi inf-clojure indent-guide htmlize hi2 heroku helm-projectile haskell-mode hackernews groovy-mode graphviz-dot-mode google-translate google-contacts go-mode git-timemachine git-gutter gist flycheck find-file-in-repository evil ess erlang ensime emms emmet-mode elixir-mode elein ein edit-server ebib dpaste dockerfile-mode dired-details dired+ d-mode csv-mode csharp-mode color-theme color-moccur cmake-mode clojure-cheatsheet c-eldoc browse-kill-ring bookmark+ bitlbee batch-mode autopair auctex ascii arduino-mode apache-mode android-mode adoc-mode ack ac-slime ac-js2 4clojure)))
+    (fancy-battery eruby-mode live-py-mode commify pyvirtualenv circe ox-reveal paradox js2-mode zenburn-theme yaml-mode web-mode virtualenvwrapper virtualenv tuareg textile-mode smartparens smart-mode-line sly-company rinari rebox2 rainbow-mode rainbow-delimiters puppet-mode psvn powerline pony-mode pomodoro php-mode p4 org-present org-jekyll org-gcal org-bullets offlineimap notmuch noctilux-theme monky minimap memory-usage markdown-mode malabar-mode magit lua-mode log4j-mode less-css-mode kanban js-comint jedi inf-clojure indent-guide htmlize hi2 heroku helm-swoop helm-projectile haskell-mode hackernews graphviz-dot-mode google-translate google-contacts go-mode git-gutter gist flycheck find-file-in-repository feature-mode evil eval-in-repl ess erlang ensime emms emmet-mode elixir-mode elein ein edit-server ebib dpaste dockerfile-mode dired-details dired+ d-mode csv-mode csharp-mode company-jedi color-theme color-moccur cmake-mode clojure-cheatsheet clj-refactor c-eldoc browse-kill-ring bookmark+ bitlbee batch-mode autopair auctex ascii arduino-mode apache-mode android-mode adoc-mode ack 4clojure)))
  '(paradox-github-token t)
  '(safe-local-variable-values (quote ((ca-cleanup-is-enabled . t))))
  '(with-editor-emacsclient-executable "/usr/bin/emacsclient"))
