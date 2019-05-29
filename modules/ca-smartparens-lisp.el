7e384c2bd057a669e352cba316dab88366572db3
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-05-29 16:28:09 +0100

    missing poly-ein

--- modules/ca-smartparens-lisp.el
+++ modules/ca-smartparens-lisp.el
@@ -1,12 +1,13 @@
 ;;; Smartparens
 (require 'smartparens-config)
+(require 'poly-ein)
 (show-smartparens-global-mode t)
-(smartparens-global-strict-mode t)
+(smartparens-global-mode t)
 
 ;; Add smartparens-strict-mode to all sp--lisp-modes hooks. C-h v sp--lisp-modes
 ;; to customize/view this list.
 (mapc (lambda (mode)
-        (add-hook (intern (format "%s-hook" (symbol-name mode))) 'smartparens-mode))
+        (add-hook (intern (format "%s-hook" (symbol-name mode))) 'smartparens-strict-mode))
       sp--lisp-modes)
 
 ;; Conveniently set keys into the sp-keymap, limiting the keybinding to buffers
