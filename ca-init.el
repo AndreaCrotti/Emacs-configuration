cdb2bf94976f7c0fb07511b91a1bd6c0c997042f
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-04-08 18:13:19 +0100

    set package-archives more explicitly

--- ca-init.el
+++ ca-init.el
@@ -17,18 +17,13 @@
 (require 'cl)
 (require 'package)
 
-
 (package-initialize)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
 
-(add-to-list 'package-archives
-             '("org" . "https://orgmode.org/elpa/"))
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-;; (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(setq package-archives
+      '(("melpa-stable" . "http://stable.melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("gnu" . "http://elpa.gnu.org/packages/")))
 
 (require 'url-handlers)
 
