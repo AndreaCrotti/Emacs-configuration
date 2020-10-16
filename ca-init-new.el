4118df5a5f943bb2c7378261e22693e9cb1c34cc
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-10-16 20:23:06 +0100

    add aliases

--- ca-init-new.el
+++ ca-init-new.el
@@ -205,5 +205,28 @@
 (global-set-key (kbd "M-p") 'ca-prev-defun)
 (global-set-key (kbd "M-n") 'ca-next-defun)
 
+(defalias 'bb 'bury-buffer)
+(defalias 'dml 'delete-matching-lines)
+(defalias 'eb 'eval-buffer)
+(defalias 'elm 'emacs-lisp-mode)
+(defalias 'er 'eval-region)
+(defalias 'go 'google-search-it)
+(defalias 'gs 'google-search-selection)
+(defalias 'll 'load-library)
+(defalias 'qrs 'query-replace-regexp)
+(defalias 'qs 'query-replace)
+(defalias 'rs 'replace-string)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(defalias 'ys 'yas/reload-all)
+(defalias 'yv 'yas/visit-snippet-file)
+
+(defalias 'rb 'revert-buffer)
+
+(defalias 'sh 'shell)
+
+(defalias 'ws 'whitespace-mode)
+(defalias 'bu 'browse-url)
+
 (load-theme 'dracula)
 (set-frame-font "-CTDB-Fira Code-normal-normal-normal-*-13-*-*-*-d-0-iso10646-1")
