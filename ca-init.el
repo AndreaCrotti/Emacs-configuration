525ec5bb5bf7bcedbcf673bfdab6aeb5d3888a5f
Andrea Crotti <andrea.crotti.@gmail.com>
2022-04-19 17:01:40 +0100

    lsp clojure hydra

--- ca-init.el
+++ ca-init.el
@@ -115,6 +115,8 @@
 (use-package clojure-mode
   :mode (("\\.clj\\'" . clojure-mode)
          ("\\.edn\\'" . clojure-mode))
+  :bind
+  (("C-c l" . lsp-clojure-refactor-menu/body))
   :init
   (add-hook 'clojure-mode-hook #'subword-mode)
   (add-to-list 'auto-mode-alist '("\\.bb" . clojure-mode)))
@@ -229,6 +231,34 @@
 
 (use-package less-css-mode)
 (use-package log4j-mode)
+
+(defhydra lsp-clojure-refactor-menu (:color blue :hint nil)
+  "
+Threading                      Code Manip                      Namespace                       Misc
+------------------------------------------------------------------------------------------------------------------------------------------------------
+_th_: Thread first             _el_: Expand let                _cn_: Clean ns                  _cp_: Cycle privacy
+_tf_: Thread first all         _il_: Introduce let             _am_: Add missing libspec       _cc_: Cycle coll
+_tt_: Thread last              _ml_: Move to let
+_tl_: Thread last all          _ef_: Extract function
+_ua_: Unwind all               _rn_: Rename
+_uw_: Unwind thread
+"
+
+  ("cp" lsp-clojure-cycle-privacy)
+  ("cn" lsp-clojure-clean-ns)
+  ("cc" lsp-clojure-cycle-coll)
+  ("am" lsp-clojure-add-missing-libspec)
+  ("el" lsp-clojure-expand-let)
+  ("il" lsp-clojure-introduce-let)
+  ("ef" lsp-clojure-extract-function)
+  ("ml" lsp-clojure-move-to-let)
+  ("th" lsp-clojure-thread-first)
+  ("rn" lsp-rename)
+  ("tf" lsp-clojure-thread-first-all)
+  ("tt" lsp-clojure-thread-last)
+  ("tl" lsp-clojure-thread-last-all)
+  ("ua" lsp-clojure-unwind-all)
+  ("uw" lsp-clojure-unwind-thread))
 
 (use-package lsp-mode
   :hook ((clojure-mode . lsp)
@@ -588,6 +618,8 @@
   :config (winner-mode t))
 
 (use-package wakatime-mode)
+
+(use-package indium)
 
 (global-set-key (kbd "M-p") 'ca-prev-defun)
 (global-set-key (kbd "M-n") 'ca-next-defun)
