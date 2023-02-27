057bb5b9528c9308932b74c71e90c355440eb01d
Andrea Crotti <andrea.crotti.0@gmail.com>
2023-02-27 13:57:16 +0000

    cleanup

--- aliases.el
+++ aliases.el
@@ -1,24 +1,16 @@
-
-
 (defalias 'bb 'bury-buffer)
+(defalias 'bu 'browse-url)
 (defalias 'dml 'delete-matching-lines)
 (defalias 'eb 'eval-buffer)
 (defalias 'elm 'emacs-lisp-mode)
 (defalias 'er 'eval-region)
-(defalias 'go 'google-search-it)
-(defalias 'gs 'google-search-selection)
 (defalias 'll 'load-library)
 (defalias 'qrs 'query-replace-regexp)
 (defalias 'qs 'query-replace)
+(defalias 'rb 'revert-buffer)
 (defalias 'rs 'replace-string)
+(defalias 'sh 'shell)
+(defalias 'ws 'whitespace-mode)
 (defalias 'yes-or-no-p 'y-or-n-p)
-
 (defalias 'ys 'yas/reload-all)
 (defalias 'yv 'yas/visit-snippet-file)
-
-(defalias 'rb 'revert-buffer)
-
-(defalias 'sh 'shell)
-
-(defalias 'ws 'whitespace-mode)
-(defalias 'bu 'browse-url)
