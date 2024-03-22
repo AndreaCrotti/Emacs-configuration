f254529a63611ad96f6228269469c3bad8512eee
Andrea Crotti <andrea.crotti.0@gmail.com>
2024-03-22 16:26:40 +0000

    extra mac setting

--- lisp/os.el
+++ lisp/os.el
@@ -6,6 +6,8 @@
 (when (is-mac?)
   (setq mac-option-modifier 'alt)
   (setq mac-command-modifier 'meta)
+  ;; to still be able to use accents
+  (setq mac-right-option-modifier 'none)
   (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
   )
 
