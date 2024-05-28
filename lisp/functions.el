ff668319cfd721e8eaf48d7a81020eb66c62c915
Andrea Crotti <andrea.crotti.0@gmail.com>
2024-05-28 13:35:16 +0100

    add a few more extra functions

--- lisp/functions.el
+++ lisp/functions.el
@@ -52,4 +52,53 @@
      (file-notify-rm-watch key))
    file-notify-descriptors))
 
+(defun redis-ks ()
+  (interactive)
+  (let ((buffer (apply #'make-comint-in-buffer
+                       "redis"
+                       redis-process-buffer-name
+                       redis-cli-executable
+                       nil
+                       '("-h"
+                         "localhost"
+                         "-p"
+                         "16378"))))
+    (with-current-buffer buffer
+      (redis-cli-mode))
+    (pop-to-buffer buffer t)))
+
+(defun git-blame-line ()
+  "Runs `git blame` on the current line and
+   adds the commit id to the kill ring"
+  (interactive)
+  (let* ((line-number (save-excursion
+                        (goto-char (point-at-bol))
+                        (+ 1 (count-lines 1 (point)))))
+         (line-arg (format "%d,%d" line-number line-number))
+         (commit-buf (generate-new-buffer "*git-blame-line-commit*")))
+    (call-process "git" nil commit-buf nil
+                  "blame" (buffer-file-name) "-L" line-arg)
+    (let* ((commit-id (with-current-buffer commit-buf
+                        (buffer-substring 1 9)))
+           (log-buf (generate-new-buffer "*git-blame-line-log*")))
+      (kill-new commit-id)
+      (call-process "git" nil log-buf nil
+                    "log" "-1" "--pretty=%h   %an   %s" commit-id)
+      (with-current-buffer log-buf
+        (message "Line %d: %s" line-number (buffer-string)))
+      (kill-buffer log-buf))
+    (kill-buffer commit-buf)))
+
+(defun advice-ns-get-selection (original &rest args)
+  (let* ((actual-string (apply original args))
+         (str-length (length actual-string)))
+    (if (> str-length (* 1024 1024))
+      (let ((what-do (yes-or-no-p "The string you copied is > 1MB. Do you really want to do that?")))
+        (if what-do
+          actual-string
+          "refusing to paste that massive thing"))
+      actual-string)))
+
+(advice-add 'ns-get-selection :around 'advice-ns-get-selection)
+
 (provide 'functions)
