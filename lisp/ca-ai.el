26aa94b28b8f0fcab1dfef0fbb384a1454da188c
Andrea Crotti <andrea.crotti.0@gmail.com>
2025-06-12 15:38:40 +0100

    add two more ai package

--- lisp/ca-ai.el
+++ lisp/ca-ai.el
@@ -4,4 +4,24 @@
 
 (use-package org-ai)
 
+(use-package aider
+  :config
+  ;; For latest claude sonnet model
+  (setq aider-args '("--model" "sonnet" "--no-auto-accept-architect"))
+  (setenv "ANTHROPIC_API_KEY" anthropic-key)
+  ;; Optional: Set a key binding for the transient menu
+  (global-set-key (kbd "C-c a") 'aider-transient-menu) ;; for wider screen
+  (aider-magit-setup-transients))
+
+(use-package aidermacs
+  :bind (("C-c b" . aidermacs-transient-menu))
+  :config
+  (setenv "ANTHROPIC_API_KEY" anthropic-key)
+  :custom
+  ; See the Configuration section below
+  (aidermacs-use-architect-mode t)
+  (aidermacs-default-model "sonnet"))
+
+(use-package aider)
+
 (provide 'ca-ai)
