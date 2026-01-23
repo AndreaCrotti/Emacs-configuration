ed5cdb05acb7ed3ef15e439f2c8617471eca72c4
Andrea Crotti <andrea.crotti.0@gmail.com>
2026-01-23 21:18:55 +0000

    few more packages

--- lisp/ca-ai.el
+++ lisp/ca-ai.el
@@ -1,6 +1,3 @@
-(use-package gptel
-  :custom
-  (gptel-default-mode #'org-mode))
 
 (use-package org-ai)
 
@@ -26,5 +23,38 @@
 (use-package claude-code)
 
 (use-package eca)
+(use-package gemini)
+
+(use-package macher
+  :custom
+  ;; The org UI has structured navigation and nice content folding.
+  (macher-action-buffer-ui 'org)
+
+  :config
+  ;; Recommended - register macher tools and presets with gptel.
+  (macher-install)
+
+  ;; Recommended - enable macher infrastructure for tools/prompts in
+  ;; any buffer.  (Actions and presets will still work without this.)
+  (macher-enable)
+
+  ;; Adjust buffer positioning to taste.
+  ;; (add-to-list
+  ;;  'display-buffer-alist
+  ;;  '("\\*macher:.*\\*"
+  ;;    (display-buffer-in-side-window)
+  ;;    (side . bottom)))
+  ;; (add-to-list
+  ;;  'display-buffer-alist
+  ;;  '("\\*macher-patch:.*\\*"
+  ;;    (display-buffer-in-side-window)
+  ;;    (side . right)))
+  )
+
+(use-package gptel
+  :config
+  (require 'macher)
+  :custom
+  (gptel-default-mode #'org-mode))
 
 (provide 'ca-ai)
