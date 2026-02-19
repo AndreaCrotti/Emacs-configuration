1b4e630d6f47e7085d65e4ab202b5e5764457e29
Andrea Crotti <andrea.crotti.0@gmail.com>
2026-02-19 08:17:48 +0000

    remove macher

--- lisp/ca-ai.el
+++ lisp/ca-ai.el
@@ -2,34 +2,7 @@
 
 (use-package org-ai)
 
-(use-package claude-code)
-
 (use-package eca)
-(use-package macher
-  :custom
-  ;; The org UI has structured navigation and nice content folding.
-  (macher-action-buffer-ui 'org)
-
-  :config
-  ;; Recommended - register macher tools and presets with gptel.
-  (macher-install)
-
-  ;; Recommended - enable macher infrastructure for tools/prompts in
-  ;; any buffer.  (Actions and presets will still work without this.)
-  (macher-enable)
-
-  ;; Adjust buffer positioning to taste.
-  ;; (add-to-list
-  ;;  'display-buffer-alist
-  ;;  '("\\*macher:.*\\*"
-  ;;    (display-buffer-in-side-window)
-  ;;    (side . bottom)))
-  ;; (add-to-list
-  ;;  'display-buffer-alist
-  ;;  '("\\*macher-patch:.*\\*"
-  ;;    (display-buffer-in-side-window)
-  ;;    (side . right)))
-  )
 
 (use-package gptel
   :config
